be07169958fd84c38b9ef462c6705fde84438376
Douglas <douglasobara97@gmail.com>
2023-10-22 17:36:32 +0300

    fetch data from the mongodb database

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,23 +1,47 @@
 import React from "react";
 import RemoveBtn from "./RemoveBtn";
 import Link from "next/link";
-
 import { HiPencilAlt } from "react-icons/hi";
 
-export default function TopicsList() {
-  return (
-    <div className="p-4 border border-slate-300 flex justify-between gap-5 my-3 items-start">
-      <div className="">
-        <h2 className="font-bold text-2xl"> Topic Title</h2>
-        <div>Topic Description</div>
-      </div>
+const getTopics = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/topics`, {
+      cache: "no-store",
+    });
 
-      <div className="flex gap-2">
-        <RemoveBtn />
-        <Link href={"/editTopic/123"}>
-          <HiPencilAlt size={24} />
-        </Link>
-      </div>
-    </div>
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics", error);
+  }
+};
+
+export default async function TopicsList() {
+  const { topics } = await getTopics();
+
+  return (
+    <>
+      {topics.map(({ title, description, _id }) => (
+        <div
+          className="p-4 border border-slate-300 flex justify-between gap-5 my-3 items-start"
+          key={_id}
+        >
+          <div className="">
+            <h2 className="font-bold text-2xl"> {title}</h2>
+            <div>{description}</div>
+          </div>
+
+          <div className="flex gap-2">
+            <RemoveBtn />
+            <Link href={`/editTopic/${_id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
   );
 }
