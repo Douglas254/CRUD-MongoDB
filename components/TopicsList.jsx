881ffe6c6750dceb751e8d9618bc670b4fddc5d3
Douglas <douglasobara97@gmail.com>
2023-10-23 00:11:54 +0300

    write functionality for deleting a topic from database

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -35,7 +35,7 @@ export default async function TopicsList() {
           </div>
 
           <div className="flex gap-2">
-            <RemoveBtn />
+            <RemoveBtn id={_id} />
             <Link href={`/editTopic/${_id}`}>
               <HiPencilAlt size={24} />
             </Link>
